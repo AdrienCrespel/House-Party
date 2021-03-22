b244a01ca9a5f173c633cc3d3d516a6fc110a444
Adrien Crespel <a.crespel56@gmail.com>
2021-03-22 15:13:19 +0100

    House Party project

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,3 +1,5 @@
-CLIENT_ID = "b9b0dad5ff12436cadd18fff0e242cc9"
-CLIENT_SECRET = "349aa93f0edf41d0a40b3a1aadfb8e7b"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+from keys import *
+
+CLIENT_ID = CLIENT_ID
+CLIENT_SECRET = CLIENT_SECRET
+REDIRECT_URI = REDIRECT_URI
